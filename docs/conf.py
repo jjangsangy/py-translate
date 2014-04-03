1c7e0b8ae599e0250d7f5dc3b86f02f7bf0aab71
Sang Han <jjangsangy@gmail.com>
2014-04-03 03:24:24 -0700

    name change

--- docs/conf.py
+++ docs/conf.py
@@ -245,7 +245,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'gtranslate', 'py-translate Documentation',
+    ('index', 'py-translate', 'py-translate Documentation',
      ['Sang Han'], 1)
 ]
 
