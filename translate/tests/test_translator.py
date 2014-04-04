f2c0c1ab83dbc2344612e4cbf66d39acefa2fd4f
Sang Han <jjangsangy@gmail.com>
2014-04-04 16:07:00 -0700

    define encoding

--- translate/tests/test_translator.py
+++ translate/tests/test_translator.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 try:
     import unittest2 as unittest
 except ImportError:
@@ -18,7 +19,7 @@ class TestTranslator(unittest.TestCase):
 
     def test_love(self):
         love = translator('en', 'zh-TW', 'I love you')
-        self.assertEqual(love['sentences'][0]['trans'].decode('utf-8'), '我愛你')
+        self.assertEqual(love['sentences'][0]['trans'], '我愛你')
 
 if __name__ == '__main__':
     unittest.main()
